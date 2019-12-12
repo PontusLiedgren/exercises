0623c8117ae6be207aa15d501892cb7e64a646e1
Pontus Liedgren <pop2strong4u@gmail.com>
2019-12-12 11:39:59 +0100

    Made all to functions

--- exercise.py
+++ exercise.py
@@ -3,45 +3,46 @@
 # https://github.com/PontusLiedgren/exercises
 
 
+# imports
+import time
+import math
 
-# ##########################################
-# # övning-1
-# # imports
-# import time
-# import math
+##########################################
+# övning-1
+# huvudloop
 
-# # variabler
-# x = 0
-# NumberOn = True
+def number_print(x):
+    NumberOn = True
+    while NumberOn:
+        print(x)
+        time.sleep(1)
+        x += 1
+    return number_print
+# Ta bort kommentaren nedan för att testa funktionen
+#number_print(0)
 
-# # huvudloop
-# while NumberOn:
-#     print(x)
-#     time.sleep(1)
-#     x += 1
+#########################################
+# övning-2
+# function
+def questionify(user_input):
+    print(user_input + "?")
+    return questionify
 
-# #########################################
-# # övning-2
+# Ta bort kommentaren nedan för att testa funktionen
+#questionify("Jag heter Pontus")
 
-# # input
-# user_input = input("Säg något: ")
+#########################################
+# övning-3
+# input
 
-# # function
-# def questionify(user_input):
-#     print(user_input + "?")
-#     return questionify
 
-# questionify(user_input)
-# #########################################
-# # övning-3
-# # input
-# user_number_input = int(input("Säg ett nummer: "))
+# function
+def divisible_by_five(number_input):
+    if_divisible = number_input/5
+    print(if_divisible.is_integer())
+    return number_input
+# Ta bort kommentaren nedan för att testa funktionen
+#user_number_input = int(input("Säg ett nummer: "))
+#divisible_by_five(user_number_input)
 
-# # function
-# def divisible_by_five(number_input):
-#     if_divisible = number_input/5
-#     print(if_divisible.is_integer())
-#     return number_input
-
-# divisible_by_five(user_number_input)
-# ##########################################
+##########################################
